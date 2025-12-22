5920e400772c1a6b37013ec9feee5a241cdd3da7
orma1 <or.matsliah@gmail.com>
2025-12-22 20:20:19 +0200

    Daniel gay

---
+++ FileInterpeter.py
@@ -0,0 +1,14 @@
+#split file line by line into dictionary like so key:value
+def readConfigFile(file):
+    with open(file) as config: #open file with default closing
+        config_dict = {} #create empty dictionary
+        for line in config: #go over each line in the file
+            line = line.strip() #should make the line empty in case of whitespace
+            if line: #if line is not empty
+                try:
+                    key, value = line.split(':') #split line by semicolon into key:value
+                    config_dict[key] = value #set the value of key
+                except:
+                    print("invalid file format") #if no semicolon, the format is not ok
+        return config_dict #after finishing creating the dictionary send it back
+
